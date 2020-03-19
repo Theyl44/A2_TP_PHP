ab50090501178a2446dc0352d55e8e595d0faa61
Theyl44 <56219948+Theyl44@users.noreply.github.com>
2020-03-19 11:41:48 +0100

    exo1

--- TP6/index.php
+++ TP6/index.php
@@ -1,3 +1,13 @@
 <?php
-    echo"<h1></h1>"
+class equipe{
+    public $nom;
+    public $nbr_titre;
+    function display(){
+        echo"L'équipe $this->nom a : $this->nbr_titre titres \n";
+    }
+}
+$p1 = new equipe();
+$p1->nom = "Real Madrid";
+$p1->nbr_titre = 10;
+$p1->display();
 ?>
