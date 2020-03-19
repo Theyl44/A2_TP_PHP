dfc7f603e07e131be0926598bff4363cdd5d82b6
Theyl44 <56219948+Theyl44@users.noreply.github.com>
2020-03-19 14:10:12 +0100

    isen tp php

--- TP6/index.php
+++ TP6/index.php
@@ -2,6 +2,9 @@
 class equipe{
     private $nom;
     private $nbr_titre;
+    static $nbEquipe;
+    const cons = "Nombre d'equipe";
+
     public function display(){
         echo"L'équipe $this->nom a : $this->nbr_titre titres \n";
     }
@@ -11,16 +14,23 @@ class equipe{
     public function setNbr_titre($nbr_titre) {
         $this->nbr_titre = $nbr_titre;
     }
-    function __construct($name, $nbr_titre){
-        $this->setNbr_titre($nbr_titre);
-        $this->setName($name);
+    function __construct($newNbTitre, $newName){
+        $this->nbr_titre = $newNbTitre;
+        $this->nom = $newName;
+        self::$nbEquipe++;
     }
     public function __destruct()
     {
         echo"Destructor\n";
     }
+    static function getNbTeam(){
+        echo self::cons;
+        echo self::$nbEquipe;
+        echo "<br>";
+    }
 
 }
 $p1 = new equipe("RajaCasabianca", 0);
 $p1->display();
+$p1::getNbTeam();
 ?>
