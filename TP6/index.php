9f6ba2518ca87b4acba97c591dda99fc389b3e4f
Theyl44 <56219948+Theyl44@users.noreply.github.com>
2020-03-19 11:50:47 +0100

    exo1

--- TP6/index.php
+++ TP6/index.php
@@ -1,13 +1,26 @@
 <?php
 class equipe{
-    public $nom;
-    public $nbr_titre;
-    function display(){
+    private $nom;
+    private $nbr_titre;
+    public function display(){
         echo"L'équipe $this->nom a : $this->nbr_titre titres \n";
     }
+    public function setName($name) {
+        $this->nom = $name;
+    }
+    public function setNbr_titre($nbr_titre) {
+        $this->nbr_titre = $nbr_titre;
+    }
+    function __construct($name, $nbr_titre){
+        $this->setNbr_titre($nbr_titre);
+        $this->setName($name);
+    }
+    public function __destruct()
+    {
+        echo"Destructor\n";
+    }
+
 }
-$p1 = new equipe();
-$p1->nom = "Real Madrid";
-$p1->nbr_titre = 10;
+$p1 = new equipe("RajaCasabianca", 0);
 $p1->display();
 ?>
