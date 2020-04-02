9017c7c225060194954b1a44f5b636365ca488bc
Theyl44 <56219948+Theyl44@users.noreply.github.com>
2020-04-02 11:55:14 +0200

    php tp

---
+++ TP7/exo3.php
@@ -0,0 +1,31 @@
+<?php
+trait Un {
+    function small($text){
+        echo '<small>'.$text.'</small>';
+    }
+
+    function big($text){
+        echo '<h2>'.$text.'</h2>';
+    }
+}
+
+trait Deux{
+    function small($text){
+        echo '<i>'.$text.'</i>';
+    }
+
+    function big($text){
+        echo '<h4>'.$text.'</h4>';
+    }
+}
+
+class Text {
+    use Un,Deux {
+        Un::big insteadof Deux;
+        Deux::small insteadof Un;
+        Deux::big as gros;
+        Deux::small as petit;
+    }
+}
+
+?>
